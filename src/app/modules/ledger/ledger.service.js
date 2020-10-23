23c766cbee2bd2724d484d524db1c0d8e13c29c7
Dinh Nguyen <dinh.nguyen@fdssoft.com>
2020-10-23 18:13:49 +0700

    Add get app version function

--- src/app/modules/ledger/ledger.service.js
+++ src/app/modules/ledger/ledger.service.js
@@ -1,5 +1,8 @@
 import nem from "nem-sdk";
 var request = require('request');
+const SUPPORT_VERSION = { LEDGER_MAJOR_VERSION: 0,
+                        LEDGER_MINOR_VERSION: 0,
+                        LEDGER_PATCH_VERSION: 2}
 /** Service storing Ledger utility functions. */
 class Ledger {
 
@@ -25,8 +28,13 @@ class Ledger {
 
     // Service methods region //
 
-    createWallet(network) {
-        return this.createAccount(network, 0, "Primary")
+    async createWallet(network) {
+        let checkVersion = await this.getAppVersion();
+        if(!checkVersion){
+            throw('Not using latest NEM BOLOS app');
+        }
+        else{
+            return this.createAccount(network, 0, "Primary")
             .then((account) => ({
                 "name": "LEDGER",
                 "accounts": {
@@ -36,6 +44,7 @@ class Ledger {
             .catch((err) => {
                 throw err;
             });
+        }
     }
 
     bip44(network, index) {
@@ -45,10 +54,40 @@ class Ledger {
         return (`44'/43'/${networkId}'/${index}'/0'`);
     }
 
+    async getAppVersion(){
+        return new Promise(async (resolve) => {
+            var JSONObject = {
+                "requestType": "getAppVersion",
+            };
+            var option = {
+                url: "http://localhost:21335",
+                method: "POST",
+                json: true,
+                body: JSONObject
+            }
+            request(option, function (error, response, body) {
+                try {
+                    let appVersion = body;
+                    if (appVersion.majorVersion < SUPPORT_VERSION.LEDGER_MAJOR_VERSION) {
+                        resolve(false);
+                    } else if (appVersion.minorVersion < SUPPORT_VERSION.LEDGER_MINOR_VERSION) {
+                        resolve(false);
+                    } else if (appVersion.patchVersion < SUPPORT_VERSION.LEDGER_PATCH_VERSION) {
+                        resolve(false);
+                    } else {
+                        resolve(true);
+                    }
+                } catch (error) {
+                    resolve(error)
+                }
+            })
+        })
+    }
+
     createAccount(network, index, label) {
         alert("Follow instructions on your device. Click OK to continue.");
         const hdKeypath = this.bip44(network, index);
-        return this.getAccount(hdKeypath, network, label);
+        return this.getAccount(hdKeypath, network, label);     
     }
 
     showAccount(account) {
@@ -106,7 +145,7 @@ class Ledger {
             //Transaction with testnet and mainnet
             //Correct the signer
             transaction.signer = account.publicKey;
-
+           
             //If it is a MosaicDefinition Creation Transaction, then correct the creator
             if (transaction.type == 0x4001) {
                 transaction.mosaicDefinition.creator = account.publicKey;
@@ -135,7 +174,8 @@ class Ledger {
         return new Promise(async (resolve) => {
             var JSONObject = {
                 "requestType": "signTransaction",
-                "serializedTx": serializedTx, "hdKeypath": account.hdKeypath
+                "serializedTx": serializedTx,
+                "hdKeypath": account.hdKeypath
             };
             var option = {
                 url: "http://localhost:21335",
