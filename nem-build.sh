afe279680c5c8ffdc6eed178e628772c9930a977
TriPhan96 <tri.phan@fdssoft.com>
2020-10-30 20:37:04 +0700

    Update build flow of NanoWallet

--- nem-build.sh
+++ nem-build.sh
@@ -1,10 +1,14 @@
 #!/bin/bash -i
 
+cd ..
+rm -rf nem-ledger-bridge
+git clone https://github.com/FDS-Ledger/nem-ledger-bridge.git
+npm install -g gulp nexe@3.3.3
+cd nem-ledger-bridge && npm install && npm run build && nexe lib/index.js -o nem-ledger-bridge
+cd ../NanoWallet
+npm install
 rm -rf build/
-npm run build
-cd build/modules/nem-ledger-bridge
-nexe lib/index.js -o nem-ledger-bridge
-cd ../..
-npm install opn
+npm run compile
+mkdir build/modules && cp -rf ../nem-ledger-bridge build/modules && rm -rf ../nem-ledger-bridge
+cd build
 nexe run.js -o NanoWallet
-./NanoWallet
